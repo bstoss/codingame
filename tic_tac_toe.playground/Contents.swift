caa639576ffd5c0867990c2a32afb966c3eb4be4
Björn <bjornrossborsky-stoss@Maci.local>
2022-01-12 08:50:47 +0100

    update tic tac toe with the loop of the real game. won league

--- tic_tac_toe.playground/Contents.swift
+++ tic_tac_toe.playground/Contents.swift
@@ -1,8 +1,12 @@
 import UIKit
 
 func randomItem<T>(fromArray a: Array<T>) -> T {
-    let index = Int(arc4random_uniform(UInt32(a.count)))
-    return a[index]
+    var rng = SystemRandomNumberGenerator()
+    let randomInt = Int.random(in: 0..<a.count, using: &rng)
+    return a[randomInt]
+    // does not work on server side swift
+//    let index = Int(arc4random_uniform(UInt32(a.count)))
+//    return a[index]
 }
 
 enum Mark {
@@ -206,3 +210,56 @@ let result = solver.chooseBestMove(grid: grid)
 grid.printBoard()
 print(result)
 //print(gameResult)
+
+/*
+ For the game
+ 
+
+ var grid = Grid()
+ let solver = Solver()
+ //grid.printBoard()
+
+ // game loop
+ while true {
+
+     let inputs = readLine()!.split(separator: " ")
+     let opponentRow = Int(inputs[0])!
+     let opponentCol = Int(inputs[1])!
+     
+    
+     
+     let validActionCount = Int(readLine()!)!
+    // debug("validActionCount: \(validActionCount)")
+    // debug("last opp action: \(opponentRow) \(opponentCol)")
+
+     if validActionCount > 0 {
+         for i in 0...(validActionCount-1) {
+             let inputs = readLine()!.split(separator: " ")
+             let row = Int(inputs[0])!
+             let col = Int(inputs[1])!
+             
+             //debug("valid action: \(row) \(col)")
+         }
+     }
+     
+     if opponentRow != -1 {
+         grid.turn = .Enemy
+         grid.makeMove(tile: Tile(position: (opponentRow, opponentCol), mark: .Enemy))
+         //grid.printBoard()
+         let result = solver.chooseBestMove(grid: grid)
+
+         // Write an action using print("message...")
+         // To debug: print("Debug messages...", to: &errStream)
+         let move = result.split(separator: " ")
+         grid.makeMove(tile: Tile(position: (Int(move[0])!, Int(move[1])!), mark: .Player))
+
+         print(result)
+
+     } else {
+         grid.turn = .Player
+         grid.makeMove(tile: Tile(position: (1, 1), mark: .Player))
+         grid.printBoard()
+         print("1 1")
+     }
+ }
+ */

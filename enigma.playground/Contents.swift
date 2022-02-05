6856a4de0be13300908dafd12daf0566b5ffb798
Björn <bjornrossborsky-stoss@Maci.local>
2022-02-05 11:56:36 +0100

    finish enigma

--- enigma.playground/Contents.swift
+++ enigma.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 
 func shiftMessage(withRotors rotors: [String], message: String, operation: String) -> String {
@@ -29,7 +30,7 @@ func shiftMessage(withRotors rotors: [String], message: String, operation: Strin
 
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-let operation = "DECODE"
+let operation = "ENCODE"
 let startingShift = 7
 let rotors = ["BDFHJLCPRTXVZNYEIWGAKMUSQO",
               "AJDKSIRUXBLHWTMCQGZNPYFVOE",
@@ -47,51 +48,53 @@ if operation == "ENCODE" {
         guard let startIndex = alphabet.firstIndex(of: char) else {
             continue
         }
-        let intIndex = startIndex.utf16Offset(in: alphabet)
-        if (intIndex + shift) >= 26 {
-            let sum = intIndex + shift
-            let minus = sum - (26*(Int(exactly: sum/26)!))
-            let offset = minus - intIndex
-            let index = alphabet.index(startIndex, offsetBy: offset)
-            shiftedMessage += alphabet[index...index]
-        } else {
-            let index = alphabet.index(startIndex, offsetBy: shift)
-            shiftedMessage += alphabet[index...index]
+        let startInt = startIndex.utf16Offset(in: alphabet)
+        var targetInt = startInt + shift
+        while targetInt > 26{
+            targetInt-=26
         }
+        targetInt = startInt - targetInt
+//        if (intIndex + shift) >= 26 {
+//            let sum = intIndex + shift
+//            let minus = sum - (26*(Int(exactly: sum/26)!))
+//            let offset = minus - intIndex
+//            let index = alphabet.index(startIndex, offsetBy: offset)
+//            shiftedMessage += alphabet[index...index]
+//        } else {
+//            let index = alphabet.index(startIndex, offsetBy: shift)
+//            shiftedMessage += alphabet[index...index]
+//        }
+        let targetIndex = alphabet.index(startIndex, offsetBy: targetInt)
+        let newChar = alphabet[targetIndex]
+        shiftedMessage += String(newChar)
         shift += 1
     }
 
     let rotatedMessage = shiftMessage(withRotors: rotors, message: shiftedMessage, operation: operation)
     
     print(rotatedMessage)
-    print(rotatedMessage == result)
 } else {
     let rotatedMessage = shiftMessage(withRotors: rotors, message: result, operation: operation)
-    print(rotatedMessage)
     var shiftedMessage = ""
     var shift = startingShift
+    
     for char in rotatedMessage {
+
+        let startIndex = alphabet.firstIndex(of: char)!
+        let startInt = startIndex.utf16Offset(in: alphabet)
+        var targetInt = startInt - shift
+        while targetInt < 0{
+            targetInt+=26
+        }
+        targetInt = targetInt - startInt
+        let targetIndex = alphabet.index(startIndex, offsetBy: targetInt)
         
-        guard let startIndex = alphabet.firstIndex(of: char) else {
-            continue
-        }
-        let intIndex = startIndex.utf16Offset(in: alphabet)
-        if (intIndex - shift) < 0 {
-            let sum = intIndex - shift
-            let plus = sum + (26*(Int(exactly: abs(sum)/26)!+1))
-//            let offset = plus + intIndex
-            print(sum)
-            print(plus)
-//            print(offset)
-            let index = alphabet.index(startIndex, offsetBy: plus)
-            shiftedMessage += alphabet[index...index]
-        } else {
-            let index = alphabet.index(startIndex, offsetBy: -shift)
-            shiftedMessage += alphabet[index...index]
-        }
+        let newChar = alphabet[targetIndex]
+        print(newChar)
+        shiftedMessage += String(newChar)
+
         shift += 1
     }
     
     print(shiftedMessage)
-    print(shiftedMessage == message)
 }
